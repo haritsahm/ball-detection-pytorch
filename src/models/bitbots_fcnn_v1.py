1fed5252ca15c920b22d6faecdbd20518c4134b6
harit-pc <haritsahm@gmail.com>
2021-12-31 14:27:21 +0700

    minor fix to errors, able to train

--- src/models/bitbots_fcnn_v1.py
+++ src/models/bitbots_fcnn_v1.py
@@ -44,25 +44,31 @@ class FCNNv1LitModel(LightningModule):
     def step(self, batch: Any):
         image, _, _, gtcenter_x, gtcenter_y, imgInfo = batch
         logits_x, logits_y = self.forward(image)
-        loss = self.criterion(logits_x, gtcenter_x) + self.criterion(logits_y, center_y)
+        gtcenter_x = torch.stack(gtcenter_x, dim=0)
+        gtcenter_y = torch.stack(gtcenter_y, dim=0)
+        arg_gt_x = torch.argmax(gtcenter_x, dim=1)  # (N, 1)
+        arg_gt_y = torch.argmax(gtcenter_y, dim=1)  # (N, 1)
+
+        loss = self.criterion(logits_x, arg_gt_x) + self.criterion(logits_y, arg_gt_y)
         preds = F.softmax(logits_x, dim=1), F.softmax(logits_y, dim=1)
 
-        return loss, preds, gtcenter_x, gtcenter_y, imgInfo
+        return loss, preds, arg_gt_x, arg_gt_y, imgInfo
 
     def training_step(self, batch: Any, batch_idx: int):
         loss, preds, gtcenter_x, gtcenter_y, imgInfo = self.step(batch)
 
         arg_pred_x = torch.argmax(preds[0], dim=1)  # (N, 1)
         arg_pred_y = torch.argmax(preds[1], dim=1)  # (N, 1)
-        arg_gt_x = torch.argmax(gtcenter_x, dim=1)  # (N, 1)
-        arg_gt_y = torch.argmax(gtcenter_y, dim=1)  # (N, 1)
+
+        print(arg_pred_x)
+        print(gtcenter_x)
 
         # log train metrics
-        acc = [func(arg_pred_x, arg_pred_y, arg_gt_x, arg_gt_y) for func in self.train_radacc]
+        acc = [func(arg_pred_x, arg_pred_y, gtcenter_x, gtcenter_y) for func in self.train_radacc]
 
         self.log("train/loss", loss, on_step=False, on_epoch=True, prog_bar=False)
         self.log("train/acc_3", acc[0], on_step=False, on_epoch=True, prog_bar=True)
-        self.log("train/acc_5", acc[2], on_step=False, on_epoch=True, prog_bar=True)
+        self.log("train/acc_5", acc[1], on_step=False, on_epoch=True, prog_bar=True)
         self.log("train/acc_10", acc[2], on_step=False, on_epoch=True, prog_bar=True)
 
         # we can return here dict with any tensors
@@ -79,15 +85,13 @@ class FCNNv1LitModel(LightningModule):
 
         arg_pred_x = torch.argmax(preds[0], dim=1)  # (N, 1)
         arg_pred_y = torch.argmax(preds[1], dim=1)  # (N, 1)
-        arg_gt_x = torch.argmax(gtcenter_x, dim=1)  # (N, 1)
-        arg_gt_y = torch.argmax(gtcenter_y, dim=1)  # (N, 1)
 
-        acc = [func(arg_pred_x, arg_pred_y, arg_gt_x, arg_gt_y) for func in self.val_radacc]
+        acc = [func(arg_pred_x, arg_pred_y, gtcenter_x, gtcenter_y) for func in self.val_radacc]
 
         # log val metrics
         self.log("val/loss", loss, on_step=False, on_epoch=True, prog_bar=False)
         self.log("val/acc_3", acc[0], on_step=False, on_epoch=True, prog_bar=True)
-        self.log("val/acc_5", acc[2], on_step=False, on_epoch=True, prog_bar=True)
+        self.log("val/acc_5", acc[1], on_step=False, on_epoch=True, prog_bar=True)
         self.log("val/acc_10", acc[2], on_step=False, on_epoch=True, prog_bar=True)
 
         return {"loss": loss, "preds": preds, "target_x": gtcenter_x, "target_y": gtcenter_y}
@@ -105,15 +109,13 @@ class FCNNv1LitModel(LightningModule):
 
         arg_pred_x = torch.argmax(preds[0], dim=1)  # (N, 1)
         arg_pred_y = torch.argmax(preds[1], dim=1)  # (N, 1)
-        arg_gt_x = torch.argmax(gtcenter_x, dim=1)  # (N, 1)
-        arg_gt_y = torch.argmax(gtcenter_y, dim=1)  # (N, 1)
 
-        acc = [func(arg_pred_x, arg_pred_y, arg_gt_x, arg_gt_y) for func in self.test_radacc]
+        acc = [func(arg_pred_x, arg_pred_y, gtcenter_x, gtcenter_y) for func in self.test_radacc]
 
         # log test metrics
         self.log("test/loss", loss, on_step=False, on_epoch=True)
         self.log("test/acc_3", acc[0], on_step=False, on_epoch=True, prog_bar=True)
-        self.log("test/acc_5", acc[2], on_step=False, on_epoch=True, prog_bar=True)
+        self.log("test/acc_5", acc[1], on_step=False, on_epoch=True, prog_bar=True)
         self.log("test/acc_10", acc[2], on_step=False, on_epoch=True, prog_bar=True)
 
         return {"loss": loss, "preds": preds, "target_x": gtcenter_x, "target_y": gtcenter_y}
@@ -134,6 +136,10 @@ class FCNNv1LitModel(LightningModule):
         See examples here:
             https://pytorch-lightning.readthedocs.io/en/latest/common/lightning_module.html#configure-optimizers
         """
-        return torch.optim.Adam(
-            params=self.parameters(), lr=self.hparams.lr, weight_decay=self.hparams.weight_decay
-        )
+        optimizer = torch.optim.Adam(
+            params=self.parameters(), lr=self.hparams.lr, weight_decay=self.hparams.weight_decay)
+
+        def lmbda(epoch): return 0.9975 ** epoch
+        scheduler = torch.optim.lr_scheduler.MultiplicativeLR(optimizer, lr_lambda=lmbda)
+
+        return [optimizer], [scheduler]
